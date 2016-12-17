043ddeca3fc71d2bb736449286b601bd88355599
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2016-12-17 16:36:01 +0200

    Add logging.

--- web/src/main/java/com/radioteria/controllers/BaseController.java
+++ web/src/main/java/com/radioteria/controllers/BaseController.java
@@ -1,5 +1,7 @@
 package com.radioteria.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,13 @@ import java.io.IOException;
 
 @Controller
 public class BaseController {
+
+    final private static Logger LOGGER = LoggerFactory.getLogger(BaseController.class);
+
     @RequestMapping("/")
     public void index(HttpServletResponse response) throws IOException {
+        LOGGER.info("Printing 'Hello, World!'");
         response.getWriter().print("Hello, World!");
     }
+
 }
