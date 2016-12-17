599759106ce0789453ee7e6cab47df051d953561
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2016-12-17 12:44:51 +0200

    Implement First Controller.

---
+++ web/src/main/java/com/radioteria/controllers/BaseController.java
@@ -0,0 +1,15 @@
+package com.radioteria.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+public class BaseController {
+    @RequestMapping("/")
+    public void index(HttpServletResponse response) throws IOException {
+        response.getWriter().print("Hello, World!");
+    }
+}
