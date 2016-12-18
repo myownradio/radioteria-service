0dba1beb18312cf92e0d4e912a6709343ac0561a
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2016-12-18 22:25:05 +0200

    Add findByPropertyValue method.

--- data/src/main/java/com/radioteria/domain/dap/AbstractDao.java
+++ data/src/main/java/com/radioteria/domain/dap/AbstractDao.java
@@ -14,6 +14,7 @@ import javax.persistence.criteria.Root;
 import java.io.Serializable;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 @Repository
 abstract class AbstractDao<P extends Serializable, E> {
@@ -57,20 +58,28 @@ abstract class AbstractDao<P extends Serializable, E> {
         return  (E) getCurrentSession().get(getEntityClass(), primaryKey);
     }
 
-    public List<E> getListByCriteria(Consumer<CriteriaBuilder> criteriaBuilderCallback) {
-        return getTypedQueryByCriteria(criteriaBuilderCallback).getResultList();
+    public E findByPropertyValue(String propertyName, String propertyValue) {
+        return findByCriteria(criteriaBuilder -> {
+            CriteriaQuery<E> criteriaQuery = criteriaBuilder.createQuery(entityClass);
+            Root<E> entityRoot = criteriaQuery.from(entityClass);
+            criteriaQuery.select(entityRoot);
+            criteriaQuery.where(criteriaBuilder.equal(entityRoot.get(propertyName), propertyValue));
+            return criteriaQuery;
+        });
     }
 
-    public E findByCriteria(Consumer<CriteriaBuilder> criteriaBuilderCallback) {
-        return getTypedQueryByCriteria(criteriaBuilderCallback).getSingleResult();
+    public E findByCriteria(Function<CriteriaBuilder, CriteriaQuery<E>> criteriaCallback) {
+        return getTypedQueryByCriteria(criteriaCallback).getSingleResult();
     }
 
-    private TypedQuery<E> getTypedQueryByCriteria(Consumer<CriteriaBuilder> criteriaBuilderCallback) {
-        Assert.notNull(criteriaBuilderCallback);
+    public List<E> getListByCriteria(Function<CriteriaBuilder, CriteriaQuery<E>> criteriaCallback) {
+        return getTypedQueryByCriteria(criteriaCallback).getResultList();
+    }
 
+    private TypedQuery<E> getTypedQueryByCriteria(Function<CriteriaBuilder, CriteriaQuery<E>> criteriaCallback) {
         CriteriaBuilder criteriaBuilder = getCurrentSession().getCriteriaBuilder();
-        criteriaBuilderCallback.accept(criteriaBuilder);
-        CriteriaQuery<E> query = criteriaBuilder.createQuery(entityClass);
+
+        CriteriaQuery<E> query = criteriaCallback.apply(criteriaBuilder);
 
         Root<E> from = query.from(entityClass);
         query.select(from);
