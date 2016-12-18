c8725d1fe70a342d2c27e0798901ad0344c4a3ab
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2016-12-18 16:30:55 +0200

    Add First Controller Test.

--- web/src/main/java/com/radioteria/controllers/BaseController.java
+++ web/src/main/java/com/radioteria/controllers/BaseController.java
@@ -1,7 +1,5 @@
 package com.radioteria.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,11 +9,8 @@ import java.io.IOException;
 @Controller
 public class BaseController {
 
-    final private static Logger LOGGER = LoggerFactory.getLogger(BaseController.class);
-
     @RequestMapping("/")
     public void index(HttpServletResponse response) throws IOException {
-        LOGGER.info("Printing 'Hello, World!'");
         response.getWriter().print("Hello, World!");
     }
 
