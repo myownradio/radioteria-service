83a2f32f3f71ad884bafd74be90f47853b7ac961
Roman Gemini <roman@homefs.biz>
2017-01-09 13:16:44 +0200

    Fucking with Jade Engine.

--- web/src/main/java/com/radioteria/utils/jade/JadeUtil.java
+++ web/src/main/java/com/radioteria/utils/jade/JadeUtil.java
@@ -2,14 +2,13 @@ package com.radioteria.utils.jade;
 
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
-import org.springframework.expression.ParserContext;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 public class JadeUtil {
 
     public Object eval(String expression) {
         ExpressionParser parser = new SpelExpressionParser();
-        Expression exp = parser.parseExpression(expression, ParserContext.TEMPLATE_EXPRESSION);
+        Expression exp = parser.parseExpression(expression);
         return exp.getValue();
     }
 
