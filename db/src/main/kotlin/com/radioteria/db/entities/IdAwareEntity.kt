03cff4f73893efd9435b295adc891ed979ae0a57
Roman <roman.lakhtadyr@gmail.com>
2017-01-21 13:10:04 +0200

    Refactoring metadata.

--- db/src/main/kotlin/com/radioteria/db/entities/IdAwareEntity.kt
+++ db/src/main/kotlin/com/radioteria/db/entities/IdAwareEntity.kt
@@ -3,12 +3,11 @@ package com.radioteria.db.entities
 import java.io.Serializable
 import javax.persistence.*
 
-const val ID_FIELD = "id"
 
 @MappedSuperclass
 abstract class IdAwareEntity<K : Serializable>(
         @Id
         @GeneratedValue
-        @Column(name = ID_FIELD)
+        @Column(name = "id")
         var id: K? = null
 )
